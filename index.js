9901b042bf4c9012746b6d69b896265bef9ef4bf
brthema072 <brunomarthe@hotmail.com>
2025-06-29 12:47:05 -0300

    Feat: Get worker

--- index.js
+++ index.js
@@ -4,31 +4,35 @@ const resultado = document.getElementById('resultado')
 btn.addEventListener('click', function () {
   resultado.value = 'Processando...'
 
-  chrome.tabs.captureVisibleTab(null, { format: 'png' }, function (dataUrl) {
+  chrome.tabs.captureVisibleTab(null, { format: 'png' }, async function (dataUrl) {
     resultado.value = 'Processando OCR...'
 
     if (chrome.runtime.lastError) {
       resultado.value = 'Erro ao capturar: ' + chrome.runtime.lastError.message
       return
     }
+    resultado.value = 'Processando OCR...'
 
-    Tesseract.recognize(
-      dataUrl,
-      'eng', // ou 'eng' para inglês
-      {
-        logger: (m) => {
-          alert(m)
-        },
-      },
-    )
-      .then(function ({ data: { text } }) {
-        resultado.value = 'olá'
-      })
-      .catch(function (err) {
-        resultado.value = 'Erro no OCR: ' + err
-      })
-      .finally(function () {
-        resultado.value = 'fim'
-      })
+    const { createWorker } = Tesseract
+
+    console.log(createWorker)
+
+    const worker = Tesseract.createWorker({
+      workerPath: chrome.runtime.getURL('tesseract/worker.min.js'),
+      langPath: chrome.runtime.getURL('tesseract/'),
+      corePath: chrome.runtime.getURL('tesseract/tesseract-core.wasm.js'),
+    })
+
+    await worker.load()
+    await worker.loadLanguage('eng')
+    await worker.initialize('eng')
+
+    const {
+      data: { text },
+    } = await worker.recognize(dataUrl)
+
+    resultado.value = text
+
+    await worker.terminate()
   })
 })
